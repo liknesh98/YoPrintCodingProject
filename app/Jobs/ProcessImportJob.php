a94aa83e661391e648a822a1fe83d19120a34b1d
liknesh98 <likneshrao@gmail.com>
2025-05-07 04:46:37 +0800

    Enabled logging for debugging purpose.

--- app/Jobs/ProcessImportJob.php
+++ app/Jobs/ProcessImportJob.php
@@ -3,18 +3,19 @@
 namespace App\Jobs;
 use App\Imports\ProductsImport;
 use App\Models\FileUpload; 
-use App\Models\Imports; 
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
-use League\Csv\Reader;
-use Maatwebsite\Excel\Excel; 
+use Illuminate\Queue\SerializesModels; 
+use Maatwebsite\Excel\Facades\Excel;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 
 class ProcessImportJob implements ShouldQueue
 {
     use Queueable,Dispatchable,InteractsWithQueue,SerializesModels;
+    public $timeout = 300;
 
     public $uploadId ; 
     /**
@@ -31,16 +32,33 @@ class ProcessImportJob implements ShouldQueue
     public function handle(): void
     {
         $upload = FileUpload::find($this->uploadId); 
-        $upload->update(['status' => 'processing']); 
+        $upload->update(['status' => 'processed']); 
 
         $path = storage_path('app/'.$upload->file_path);
-
+            Log::info($upload->type);
             if($upload->type === 'imports'){
-                Excel::import(new ProductsImport,$path);
+                try{
+
+                    Log::info('Starting import from: ' . $upload->file_path);
+                    Excel::import(new ProductsImport,  $upload->file_path, 'local');
+                }
+                catch (\Throwable $e) 
+                {
+                    \Log::error('Import job failed', [
+                        'error' => $e->getMessage(),
+                        'upload_id' => $this->uploadId,
+                        'path' => $upload->file_path,
+                    ]);
+                
+                    $upload->update(['status' => 'failed']);
+                
+                    throw $e; // rethrow so Horizon can mark it as failed
+                }
+                
             }
 
             //Other model processors can be added here for scaling purposes. 
-       
+        Log::info('✅ Import completed for: ' . $upload->file_path);
         $upload->update(['status'=>'completed']);
     }
 
