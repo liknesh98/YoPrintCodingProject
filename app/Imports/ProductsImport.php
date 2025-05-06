5fe8264d2f22b1968e69e1fa7d6e7294ab95b204
liknesh98 <likneshrao@gmail.com>
2025-05-07 04:45:14 +0800

    logging enabled for debugging.

--- app/Imports/ProductsImport.php
+++ app/Imports/ProductsImport.php
@@ -5,7 +5,7 @@ namespace App\Imports;
 use App\Models\Imports;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
-
+use Illuminate\Support\Facades\Log;
 
 class ProductsImport implements ToCollection
 {
@@ -19,17 +19,27 @@ class ProductsImport implements ToCollection
      
         $header =$rows->first(); 
         $rows = $rows->skip(1); 
-
+        Log::info('Rows received:', ['count' => $rows->count()]);
         foreach ($rows as $row) 
-        {
+        {    
+            Log::info('Row:', $row->toArray());
+            try {
             $rawRow = array_combine($header->toArray(),$row->toArray());
             $rowData =array_map(function ($value){
                 return mb_convert_encoding($value,'UTF-8','UTF-8');
             },$rawRow);
-            Imports::updateOrInsert(
-                ['UNIQUE_KEY'=> $rowData['UNIQUE_KEY']],
-                $rowData
-            );
+           
+                Imports::updateOrInsert(
+                    ['UNIQUE_KEY'=> $rowData['UNIQUE_KEY']],
+                    $rowData
+                );
+            } catch (\Throwable $e) {
+                Log::error('Failed to insert row', [
+                    'row' => $row->toArray(),
+                    'error' => $e->getMessage()
+                ]);
+            }
+           
         }
     }
 }
