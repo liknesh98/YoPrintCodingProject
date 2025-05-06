d2d5943071d04f3d922d78152d4cc2c1707ddae5
liknesh98 <likneshrao@gmail.com>
2025-05-06 23:44:22 +0800

    Added horizon route in web route

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('dashboard');
 })->name('dashboard');
+Route::get('/horizon', function () {
+    return redirect('/horizon/dashboard');
+});
 
 Route::get('/upload/', [UploadController::class, 'index'])->name('uploadPage'); // Generate participants
