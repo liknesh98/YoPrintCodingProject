edfae6d1c8feee09090b8b99c6999bc4a69c4d56
liknesh98 <likneshrao@gmail.com>
2025-05-07 04:44:38 +0800

    changed upload type and included status update method in the controller.

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\FileUpload;
 use Illuminate\Http\Request;
 use App\Services\ImportService;
 
@@ -11,11 +11,29 @@ class UploadController extends Controller
         return view('upload') ; 
     }     
     public function upload(Request $request)
-{
+    {
     $request->validate(['file' => 'required|file|mimes:csv']);
 
-    $upload = ImportService::handle($request->file('file'), 'products');
+    $upload = ImportService::handle($request->file('file'), 'imports');
 
     return back()->with('success', 'File uploaded and queued!');
-}
+    }
+
+    public function status()
+    {
+        $uploads = FileUpload::latest()->take(10)->get();
+
+        return response()->json([
+            'data' => $uploads->map(function ($upload){
+                return [
+                    'file_name' => $upload->file_name,
+                    'created_at' => $upload->created_at->format('Y-m-d H:i:s'),
+                    'human_time' => $upload->created_at->diffForHumans(),
+                    'status' => ucfirst($upload->status),
+                ];
+            }),
+        ]);
+    }
+
+
 }
