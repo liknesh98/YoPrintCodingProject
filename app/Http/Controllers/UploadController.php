2310f2602e649d670e78b1cb37c1aa02fa2b3318
liknesh98 <likneshrao@gmail.com>
2025-05-07 02:14:05 +0800

    Created a function that invokes the service.

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\ImportService;
 
 class UploadController extends Controller
 {
     public function index () {
         return view('upload') ; 
     }     
+    public function upload(Request $request)
+{
+    $request->validate(['file' => 'required|file|mimes:csv']);
+
+    $upload = ImportService::handle($request->file('file'), 'products');
+
+    return back()->with('success', 'File uploaded and queued!');
+}
 }
